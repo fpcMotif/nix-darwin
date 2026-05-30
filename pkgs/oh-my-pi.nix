770bedbc945e7e1a9e3808a98374cd6fa740fc86
fpcmotif <fpcmotif@gmail.com>
2026-05-30 12:47:09 +0800

    chore: bump pi and oh-my-pi binaries

--- pkgs/oh-my-pi.nix
+++ pkgs/oh-my-pi.nix
@@ -22,11 +22,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "oh-my-pi";
-  version = "15.5.11";
+  version = "15.5.15";
 
   src = fetchurl {
     url = "https://github.com/can1357/oh-my-pi/releases/download/v${finalAttrs.version}/omp-darwin-arm64";
-    hash = "sha256-owrTpO4x+LVKyTKMTL5uG5LF/hrlqDWheTNDp2f42Dk=";
+    hash = "sha256-Pp0CNGzYRr2zuZ9axT2melv07FFqfdkohfYQQmvSjYg=";
   };
 
   dontUnpack = true;
--- pkgs/pi-coding-agent.nix
+++ pkgs/pi-coding-agent.nix
@@ -39,11 +39,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "pi-coding-agent";
-  version = "0.77.0";
+  version = "0.78.0";
 
   src = fetchurl {
     url = "https://github.com/badlogic/pi-mono/releases/download/v${finalAttrs.version}/pi-darwin-arm64.tar.gz";
-    hash = "sha256-BZF64fvEchD9JhQwqSRqN+nGDXwIUoKY9er3t74Ky0w=";
+    hash = "sha256-aOu+T1ahNqHHus4zk+ykrQqh/Z8lO3l/03AFi9Of4HA=";
   };
 
   sourceRoot = "pi";
